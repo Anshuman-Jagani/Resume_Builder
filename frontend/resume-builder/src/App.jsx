44c318faeda11d6ec1799a8392a66e79b4ef0a00
Anshuman Jagani <anshuman.r.1797@gmail.com>
2025-05-07 22:20:47 +0530

    Landing Page

--- frontend/resume-builder/src/App.jsx
+++ frontend/resume-builder/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import LandingPage from "./pages/LandingPage";
+import Dashborad from './pages/Home/Dashborad';
+import EditResue from './pages/ResumeUpdate/EditResue';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+    <div>
+      <Router>
+        <Routes>
+          { /*Default Route */ }
+          <Route path="/" element={< LandingPage />} />
+          <Route path="/dashboard" element={< Dashborad />} />
+          <Route path="/resume/:resumeId" element={< EditResue />} />
+        </Routes>
+      </Router>
+    </div>
 
-export default App
+    <Toaster
+      toastOptions={{
+        className: "",
+        style: {
+          fontSize: "13px",
+        },
+      }}
+    />
+    </>
+  );
+};
+
+export default App;
